f6a2a9717a5e1bd514b2d26871c5a8cd6469fb77
ASlugin <slugin.alexander@gmail.com>
2023-02-18 01:00:34 +0300

    Add comment and fix printfn

--- Homework1/Factorial/Factorial/Program.fs
+++ Homework1/Factorial/Factorial/Program.fs
@@ -3,10 +3,10 @@
 let input = Console.ReadLine()
 let intValue = input |> int
 
+// Counts factorial of the number
 let rec factorial number =
     if number = 0 then 1
     elif number = 1 then 1
     else number * factorial (number - 1)
-   
-let result = factorial intValue
-printfn "%d" result
+    
+printfn $"%d{factorial intValue}"
