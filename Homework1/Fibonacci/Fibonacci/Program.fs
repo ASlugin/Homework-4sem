c163a306bd36c613d65d4d428a4e089bb6911523
ASlugin <slugin.alexander@gmail.com>
2023-02-18 00:40:04 +0300

    Add solution for fibonacci task

---
+++ Homework1/Fibonacci/Fibonacci/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let input = Console.ReadLine()
+let intValue = input |> int
+
+// Counts Fibonacci number
+let fibonacci number =
+    let rec fibonacciCompute number current next =
+        if number = 0 then current
+        else fibonacciCompute (number - 1) next (current + next)
+        
+    fibonacciCompute number 0 1
+
+printfn $"%d{fibonacci intValue}"
