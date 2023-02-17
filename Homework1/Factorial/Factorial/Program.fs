587626e28350cb3744734c70fa7966dcac6aec68
ASlugin <slugin.alexander@gmail.com>
2023-02-18 00:06:16 +0300

    Add solution for factorial task

---
+++ Homework1/Factorial/Factorial/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let input = Console.ReadLine()
+let intValue = input |> int
+
+let rec factorial number =
+    if number = 0 then 1
+    elif number = 1 then 1
+    else number * factorial (number - 1)
+   
+let result = factorial intValue
+printfn "%d" result
