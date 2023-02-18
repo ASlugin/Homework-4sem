ab67fbd8c21e11f8f3e706fa7db80a3b38b69654
ASlugin <slugin.alexander@gmail.com>
2023-02-18 13:15:28 +0300

    Add solution for reverse list task

---
+++ Homework1/ReverseList/ReverseList/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+printfn "%s" "Enter length of list:"
+let length = Console.ReadLine() |> int
+printfn "%s" "Enter list items in one line:"
+let input = Console.ReadLine().Split(" ")
+let list = List.init length (fun i -> input[i] |> int)
+
+// Returns new list with items in reverse order
+let reverseList list =
+    let newList = []
+    
+    let rec recursiveReverseList list newList indexOfElement =
+        if indexOfElement >= List.length list then newList
+        else recursiveReverseList list (list.Item(indexOfElement) :: newList) (indexOfElement + 1)
+    
+    recursiveReverseList list newList 0
+
+printfn $"%A{reverseList list}"
