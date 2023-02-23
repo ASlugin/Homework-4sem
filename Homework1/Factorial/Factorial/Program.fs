b77bf07fb6f39d675daa8e2e0fe7d6c4babf68bf
ASlugin <slugin.alexander@gmail.com>
2023-02-23 14:14:12 +0300

    Fix bugs

--- Homework1/Factorial/Factorial/Program.fs
+++ Homework1/Factorial/Factorial/Program.fs
@@ -3,10 +3,15 @@
 let input = Console.ReadLine()
 let intValue = input |> int
 
-// Counts factorial of the number
-let rec factorial number =
-    if number = 0 then 1
-    elif number = 1 then 1
-    else number * factorial (number - 1)
+// Counts factorial of the number; if number < 0, throws ArgumentException
+let factorial number =
+    if number < 0 then raise(ArgumentException "Number less than zero")
+    
+    let rec countFactorial number multiplier =
+        if number = 0 then 1
+        elif number = 1 then multiplier
+        else countFactorial (number - 1) (number * multiplier)
+        
+    countFactorial number 1
     
 printfn $"%d{factorial intValue}"
