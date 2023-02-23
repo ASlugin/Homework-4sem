b0f3999f927376b1ad653dbed4a12996d46ed957
ASlugin <slugin.alexander@gmail.com>
2023-02-23 16:09:23 +0300

    Fix bug

--- Homework1/FindInList/FindInList/Program.fs
+++ Homework1/FindInList/FindInList/Program.fs
@@ -3,19 +3,21 @@
 // Finds number in list and returns first index of the number in list
 // If there is not number in list, returns -1
 let findInList list number =
-    let rec recursiveFind list number index =
-        if index >= List.length list then -1
-        elif number = list.Item(index) then index
-        else recursiveFind list number (index + 1)
+    let rec find number list index =
+        match list with
+        | [] -> -1
+        | head :: rest ->
+            if head = number then index
+            else find number rest (index + 1)
+            
+    find number list 0
     
-    recursiveFind list number 0
-
-printf "%s" "Enter length of list: "
+printf "Enter length of list: "
 let length = Console.ReadLine() |> int
-printf "%s" "Enter list items in one line: "
+printf "Enter list items in one line: "
 let input = Console.ReadLine().Split(" ")
 let list = List.init length (fun i -> input[i] |> int)
-printf "%s" "Enter number to find in list: "
+printf "Enter number to find in list: "
 let number = Console.ReadLine() |> int
 
 printf $"Index of the number in list: %d{findInList list number}"
