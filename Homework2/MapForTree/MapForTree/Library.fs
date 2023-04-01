46071d2d408949dbdad06d00a14f6b5f6b234b3c
ASlugin <slugin.alexander@gmail.com>
2023-04-01 14:56:39 +0300

    Add tree map task

---
+++ Homework2/MapForTree/MapForTree/Library.fs
@@ -0,0 +1,12 @@
+﻿module MapForTree
+    
+type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+    | Empty
+    
+let rec mapTree func tree =
+    match tree with
+    | Tree(node, leftTree, rightTree) -> Tree(func node, mapTree func leftTree, mapTree func rightTree)
+    | Tip node -> Tip(func node)
+    | Empty -> Empty
