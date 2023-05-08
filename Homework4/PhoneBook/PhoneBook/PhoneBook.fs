197d6a04511816814f82778e03bfff185d082c6a
ASlugin <slugin.alexander@gmail.com>
2023-05-08 19:21:33 +0300

    Add phone book

---
+++ Homework4/PhoneBook/PhoneBook/PhoneBook.fs
@@ -0,0 +1,47 @@
+﻿module PhoneBook
+
+open System.IO
+
+type Phone = {Name: string; Number: string;}
+
+// Returns new database and true if it was new number, else false 
+let addNewEntry (database : list<Phone>) name number =
+    let numberIsExist = database |> List.exists (fun entry -> entry.Number = number)
+    if not numberIsExist then
+        let newEntry = { Name = name; Number = number }
+        newEntry::database, true
+    else
+        database, false
+
+// Returns option with number
+let findNumberByName (database : list<Phone>) name =
+    let numberOption = database |> List.tryFind (fun entry -> entry.Name = name)
+    match numberOption with
+    | Some value -> Some value.Number
+    | None -> None
+    
+// Returns option with name
+let findNameByNumber (database : list<Phone>) number =
+    let nameOption = database |> List.tryFind (fun entry -> entry.Number = number)
+    match nameOption with
+    | Some value -> Some value.Name
+    | None -> None
+
+let saveDataToFile (database : list<Phone>) =
+    use stream = new StreamWriter("output.txt")
+    database |> List.iter (fun entry -> stream.WriteLine($"{entry.Name} {entry.Number}")) 
+    ()
+
+let readDataFromFile (database : list<Phone>) =
+    use stream = new StreamReader("input.txt")
+    
+    let rec readLoop localDatabase =
+        let str = stream.ReadLine()
+        if isNull str then localDatabase
+        else
+            let splitLine = str.Split(' ')
+            if splitLine.Length <> 2 then raise (invalidArg "Name and number" "Phone book entry should have two argument")
+            let newDatabase, _ = addNewEntry localDatabase splitLine[0] splitLine[1]
+            readLoop newDatabase
+            
+    readLoop database
---
+++ Homework4/PhoneBook/PhoneBook/Program.fs
@@ -0,0 +1,72 @@
+﻿module Interface
+
+open System
+open PhoneBook
+
+let printHelp() =
+    printfn "Enter one of the next command:"
+    printfn "-1 - exit"
+    printfn "0 - help"
+    printfn "1 - add a new entry"
+    printfn "2 - find phone number by name"
+    printfn "3 - find name by phone number"
+    printfn "4 - print all entries"
+    printfn "5 - save data to file"
+    printfn "6 - read data from file"
+
+let start =
+    let rec loop database =
+        let command = Console.ReadLine()
+        match command with
+        | "-1" -> ()
+        | "0" ->
+            printHelp()
+            loop database
+        | "1" ->
+            printf "Enter the name: "
+            let name = Console.ReadLine()
+            printf "Enter the number: "
+            let number = Console.ReadLine()
+            
+            let newDatabase, itIsNewNumber = addNewEntry database name number
+            if not itIsNewNumber then
+                printfn "Such number already exist in phone book"
+            else
+                printfn "Phone number added"
+            loop newDatabase
+        | "2" ->
+            printf "Enter the name: "
+            let name = Console.ReadLine()
+            let numberOption = findNumberByName database name
+            match numberOption with
+            | Some number -> printfn $"{number}"
+            | None -> printfn $"There is no entry with name \"{name}\""
+            loop database
+        | "3" ->
+            printf "Enter the number: "
+            let number = Console.ReadLine()
+            let nameOption = findNameByNumber database number
+            match nameOption with
+            | Some name -> printfn $"{name}"
+            | None -> printfn $"There is no entry with number \"{number}\""
+            loop database
+        | "4" ->
+            printfn "All entries:"
+            database |> List.iter (fun (phone:Phone) -> printfn $"{phone.Name} {phone.Number}")
+            loop database
+        | "5" ->
+            saveDataToFile database
+            printfn "Data saved to file"
+            loop database
+        | "6" ->
+            let newDatabase = readDataFromFile database
+            printfn "Data read from file"
+            loop newDatabase
+        | _ ->
+            printfn "Unknown command"
+            loop database
+
+    printHelp()
+    loop []
+
+start
