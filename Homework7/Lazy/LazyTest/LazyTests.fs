8ada7e5561d613736b57628bd5a71e2fccbc5f11
ASlugin <slugin.alexander@gmail.com>
2023-05-26 17:23:35 +0300

    Add multi thread tests

--- Homework7/Lazy/LazyTest/LazyTests.fs
+++ Homework7/Lazy/LazyTest/LazyTests.fs
@@ -1,5 +1,6 @@
 module LazyTest
 
+open System.Threading
 open NUnit.Framework
 open FsUnit
 open Lazy.LazyFactory
@@ -32,3 +33,23 @@ type OneThreadLazyTests() =
         (simpleLazy.Get(), threadSafeLazy.Get(), lockFreeLazy.Get()) |> should equal (107, 107, 107)
         (simpleLazy.Get(), threadSafeLazy.Get(), lockFreeLazy.Get()) |> should equal (107, 107, 107)
         callCount |> should equal 3
+        
+
+[<Test>]
+let multiThreadLazyTest () =
+    let mutable a = 12345
+    let supplier () =
+        a <- 54321
+        Thread.Sleep(100)
+        a
+        
+    let lockFreeLazy = LazyFactory.CreateLockFreeLazy(supplier)
+    let threadSafeLazy = LazyFactory.CreateThreadSafeLazy(supplier)
+
+    let getters = Seq.init 100 (fun _ ->
+        async { lockFreeLazy.Get() |> should equal 54321
+                threadSafeLazy.Get() |> should equal 54321 }
+        )
+    
+    getters |> Async.Parallel |> Async.RunSynchronously |> ignore
+    a |> should equal 54321
