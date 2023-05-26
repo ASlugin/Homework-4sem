9ed484821bce6af2b7e694677569ef07f5c60e09
ASlugin <slugin.alexander@gmail.com>
2023-05-26 13:23:59 +0300

    Add worklow tasks

---
+++ Homework4/Workflow/Workflow/Builders.fs
@@ -0,0 +1,15 @@
+﻿module Builders
+
+open System
+
+type RounderBuilder (digits: int) =
+    member this.Bind(x, func) = func x
+    member this.Return(x: float) = Math.Round(x, digits)
+
+type StringCalculatorBuilder () =
+    member this.Bind(x: string, func) = 
+        try 
+            x |> int |> func
+        with :? FormatException
+            -> None
+    member this.Return(x) = Some x
